e62a933efd91fd73d72188cd915c9ce6a6a8429a
Ruchith4560 <macharuchith1@gmail.com>
2026-03-06 21:45:10 +0530

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ app = FastAPI()
 origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
