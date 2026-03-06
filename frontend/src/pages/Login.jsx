65fe1f9bafefdce77410c11b06e526421d8a4d9b
Ruchith4560 <macharuchith1@gmail.com>
2026-03-06 22:53:17 +0530

    modified

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../services/api";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 function Login() {
   const [email, setEmail] = useState("");
